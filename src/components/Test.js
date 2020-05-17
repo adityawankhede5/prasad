9a98b3f31a77728906293568c817a7b5016a124a
adityawankhede5 <adityawankhede555@gmail.com>
2020-05-17 20:24:42 +0530

    Included fonts and made them scalable

---
+++ src/components/Test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from '../css/Fonts.module.css';
+
+function Test() {
+    return (
+        <div>
+
+            <div className={styles.merriweather}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.mwLight}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.mwBold}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.mwItalicRed}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.poppinsLight}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.poppinsLightItalic}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.poppins}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.poppinsMedium}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.poppinsSemiBold}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.poppinsBold}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+            <div className={styles.rwBold}>
+                I'm a UX designer, trying to create meaningful experiences through design and intend to solve some real life problems.
+            </div>
+
+        </div>
+    )
+}
+
+export default Test
