150a1548d3cd80f35ef5e01f499701acd125c5c4
adityawankhede5 <adityawankhede555@gmail.com>
2020-05-30 23:55:47 +0530

    Updates

---
+++ precache-manifest.663e32364fa1c4873dc7bb584bb24a5c.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda1108eb4bfd7ec05ab02a01c5ea1e6",
+    "url": "/prasad/index.html"
+  },
+  {
+    "revision": "5805efe996eaffbb03d4",
+    "url": "/prasad/static/css/main.7cfd8158.chunk.css"
+  },
+  {
+    "revision": "7fc6dcbb7a9675ed7769",
+    "url": "/prasad/static/js/2.20861e9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prasad/static/js/2.20861e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5805efe996eaffbb03d4",
+    "url": "/prasad/static/js/main.e33d4368.chunk.js"
+  },
+  {
+    "revision": "d1828630e16472e9c738",
+    "url": "/prasad/static/js/runtime-main.9236e09f.js"
+  },
+  {
+    "revision": "52f76a5e054a88959341373dc573c029",
+    "url": "/prasad/static/media/Group 24.52f76a5e.png"
+  },
+  {
+    "revision": "03a90c9c5f35c1aebef4e26e41a12e65",
+    "url": "/prasad/static/media/Group 29.03a90c9c.png"
+  },
+  {
+    "revision": "013cc7f2f4aebc019a2059f5962d8be1",
+    "url": "/prasad/static/media/Group 32.013cc7f2.png"
+  },
+  {
+    "revision": "344545bac7bd0d0906aab0ff9c413370",
+    "url": "/prasad/static/media/Group 3343.344545ba.png"
+  },
+  {
+    "revision": "c45ad002332f8f0d3d02080ec8fcbbae",
+    "url": "/prasad/static/media/Group 3344.c45ad002.png"
+  },
+  {
+    "revision": "4dc9fc70c618c515f702942d3130bd5d",
+    "url": "/prasad/static/media/Group 3354.4dc9fc70.png"
+  },
+  {
+    "revision": "abe059a1697cf9dcee2524417cceeec1",
+    "url": "/prasad/static/media/analysis.abe059a1.svg"
+  },
+  {
+    "revision": "04f4396fd96ec34073ded8ba38e82264",
+    "url": "/prasad/static/media/analysisDesktop.04f4396f.svg"
+  },
+  {
+    "revision": "6a51b753dad77c5e0ef6fd98b425e70d",
+    "url": "/prasad/static/media/analysisTable.6a51b753.svg"
+  },
+  {
+    "revision": "7ab222669342f520cf4e5ab0c89faa51",
+    "url": "/prasad/static/media/arrowleft.7ab22266.svg"
+  },
+  {
+    "revision": "eae6d8db0ce3636574b5dfae4161525e",
+    "url": "/prasad/static/media/arrowright.eae6d8db.svg"
+  },
+  {
+    "revision": "e1b031014568d0092434c47992a2689a",
+    "url": "/prasad/static/media/behance.e1b03101.svg"
+  },
+  {
+    "revision": "4b46940160ae6a363c470ce81fa5cf42",
+    "url": "/prasad/static/media/bishop.4b469401.png"
+  },
+  {
+    "revision": "7eea75003d9977d0f3f0f1684b73d27d",
+    "url": "/prasad/static/media/bishopbg.7eea7500.svg"
+  },
+  {
+    "revision": "3ba78b693f6043c0fa5928c2eff5b482",
+    "url": "/prasad/static/media/bishopbgdesktop.3ba78b69.svg"
+  },
+  {
+    "revision": "27ccd1cabca2a199ddce3a2e8634a255",
+    "url": "/prasad/static/media/certi.27ccd1ca.svg"
+  },
+  {
+    "revision": "450987a4a152d4db6c6a8124ef76bcc6",
+    "url": "/prasad/static/media/chessbg.450987a4.jpg"
+  },
+  {
+    "revision": "a4e7f513e2c8d333d2939c49271113ff",
+    "url": "/prasad/static/media/chessintro.a4e7f513.jpg"
+  },
+  {
+    "revision": "6f47bd7a6053f39cf2c1b0eb55371f62",
+    "url": "/prasad/static/media/controls.6f47bd7a.svg"
+  },
+  {
+    "revision": "e0f0c88ae6f0b01e36dc358decc44fa1",
+    "url": "/prasad/static/media/homepagebg.e0f0c88a.svg"
+  },
+  {
+    "revision": "87c21af71bffd0bd62356fa15e63255c",
+    "url": "/prasad/static/media/king.87c21af7.png"
+  },
+  {
+    "revision": "81f33f27c16073d8da74dae56bdf8fb2",
+    "url": "/prasad/static/media/kingbg.81f33f27.svg"
+  },
+  {
+    "revision": "984c23dbcc1150607c719ffcc51d0f5f",
+    "url": "/prasad/static/media/kingbgdesktop.984c23db.svg"
+  },
+  {
+    "revision": "fd8b81b00d6c5045cf2e1fb489c5229b",
+    "url": "/prasad/static/media/knight.fd8b81b0.png"
+  },
+  {
+    "revision": "3d5a334c946f2ecd0ebd1ba3dd4b329a",
+    "url": "/prasad/static/media/knightbg.3d5a334c.svg"
+  },
+  {
+    "revision": "fc494055a59b08e6d2746472da923ee3",
+    "url": "/prasad/static/media/linkedin.fc494055.svg"
+  },
+  {
+    "revision": "5a275c2e4b286b91ed9a0a561e0db869",
+    "url": "/prasad/static/media/me.5a275c2e.png"
+  },
+  {
+    "revision": "2efa8078fa3b64d728080d5962e13849",
+    "url": "/prasad/static/media/pawn.2efa8078.png"
+  },
+  {
+    "revision": "f0e422179ed97c429dab9734f9ea04e5",
+    "url": "/prasad/static/media/playchess.f0e42217.jpg"
+  },
+  {
+    "revision": "263b0d99a4e2046eec68813a5446865d",
+    "url": "/prasad/static/media/queen.263b0d99.png"
+  },
+  {
+    "revision": "f79a3388748aef5f4c957795266200b7",
+    "url": "/prasad/static/media/queenbg.f79a3388.svg"
+  },
+  {
+    "revision": "fd08d1f342814bf5ac8b73461de8037d",
+    "url": "/prasad/static/media/queenbgdesktop.fd08d1f3.svg"
+  },
+  {
+    "revision": "80f67c60a25b713b76f13f766a8b52af",
+    "url": "/prasad/static/media/reviews.80f67c60.png"
+  },
+  {
+    "revision": "47617163cf7e04216da83a27d3c22e93",
+    "url": "/prasad/static/media/rock.47617163.png"
+  },
+  {
+    "revision": "6de4147f8e3ac4c7cb256b53085ab508",
+    "url": "/prasad/static/media/rock1.6de4147f.jpg"
+  },
+  {
+    "revision": "c271e5651f5544acb65e9ec51ed976a3",
+    "url": "/prasad/static/media/rock2.c271e565.jpg"
+  },
+  {
+    "revision": "d4fc4362eae5dc6ad82260e499ee20ca",
+    "url": "/prasad/static/media/rock3.d4fc4362.jpg"
+  },
+  {
+    "revision": "31fbc46c77d0896412b0ce044d3e3bea",
+    "url": "/prasad/static/media/rock4.31fbc46c.jpg"
+  },
+  {
+    "revision": "0edbeff82ab20088b4b99e3390159c91",
+    "url": "/prasad/static/media/sketch1.0edbeff8.jpg"
+  },
+  {
+    "revision": "c447d8765bfd78ad847c8050f4c76ad4",
+    "url": "/prasad/static/media/sketch2.c447d876.jpg"
+  },
+  {
+    "revision": "b177b4123906c695801042b00084fad0",
+    "url": "/prasad/static/media/sketch3.b177b412.jpg"
+  },
+  {
+    "revision": "b6805ef566bf72bac3585039a908f949",
+    "url": "/prasad/static/media/sketch4.b6805ef5.jpg"
+  }
+]);
