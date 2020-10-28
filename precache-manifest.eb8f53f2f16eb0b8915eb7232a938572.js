a3851c0047e8b9f0c5bd9eb19faf3637860aa412
adityawankhede5 <adityawankhede555@gmail.com>
2020-10-28 10:28:48 +0530

    Updates

---
+++ precache-manifest.eb8f53f2f16eb0b8915eb7232a938572.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b4014d78d7ff9ba30fe524c2e69100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7bbd9a3f91c2a36b93",
+    "url": "/static/css/main.16b973cb.chunk.css"
+  },
+  {
+    "revision": "4d093aeb57f249dadf98",
+    "url": "/static/js/2.49521337.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.49521337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7bbd9a3f91c2a36b93",
+    "url": "/static/js/main.515b2a1f.chunk.js"
+  },
+  {
+    "revision": "a5718fcbe3abd8d374a4",
+    "url": "/static/js/runtime-main.461f7ebf.js"
+  },
+  {
+    "revision": "52f76a5e054a88959341373dc573c029",
+    "url": "/static/media/Group 24.52f76a5e.png"
+  },
+  {
+    "revision": "03a90c9c5f35c1aebef4e26e41a12e65",
+    "url": "/static/media/Group 29.03a90c9c.png"
+  },
+  {
+    "revision": "013cc7f2f4aebc019a2059f5962d8be1",
+    "url": "/static/media/Group 32.013cc7f2.png"
+  },
+  {
+    "revision": "344545bac7bd0d0906aab0ff9c413370",
+    "url": "/static/media/Group 3343.344545ba.png"
+  },
+  {
+    "revision": "c45ad002332f8f0d3d02080ec8fcbbae",
+    "url": "/static/media/Group 3344.c45ad002.png"
+  },
+  {
+    "revision": "4dc9fc70c618c515f702942d3130bd5d",
+    "url": "/static/media/Group 3354.4dc9fc70.png"
+  },
+  {
+    "revision": "7ab222669342f520cf4e5ab0c89faa51",
+    "url": "/static/media/arrowleft.7ab22266.svg"
+  },
+  {
+    "revision": "eae6d8db0ce3636574b5dfae4161525e",
+    "url": "/static/media/arrowright.eae6d8db.svg"
+  },
+  {
+    "revision": "e1b031014568d0092434c47992a2689a",
+    "url": "/static/media/behance.e1b03101.svg"
+  },
+  {
+    "revision": "27ccd1cabca2a199ddce3a2e8634a255",
+    "url": "/static/media/certi.27ccd1ca.svg"
+  },
+  {
+    "revision": "e0f0c88ae6f0b01e36dc358decc44fa1",
+    "url": "/static/media/homepagebg.e0f0c88a.svg"
+  },
+  {
+    "revision": "fc494055a59b08e6d2746472da923ee3",
+    "url": "/static/media/linkedin.fc494055.svg"
+  },
+  {
+    "revision": "5a275c2e4b286b91ed9a0a561e0db869",
+    "url": "/static/media/me.5a275c2e.png"
+  }
+]);
